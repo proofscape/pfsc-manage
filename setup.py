960b80d1e90ce8f7d271ca560e23faa6df6db045
Steve Kieffer <sk@skieffer.info>
2022-09-20 16:46:59 -0400

    Bump dev version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='pfsc-manage',
-    version='0.23.2',
+    version='0.23.3-dev',
     url='https://github.com/proofscape/pfsc-manage',
     py_modules=['manage'],
     install_requires=[
